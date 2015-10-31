66424600f16ab09ccec4e92c24d47db9f17ad6a1
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-10-30 21:04:53 -0500

    Backend code is finished! The helper class to refresh the caches is done and called on app open and can be called elsewhere when needed.

---
+++ TrailKeeper/GetAllObjectsFromParseHelper.h
@@ -0,0 +1,22 @@
+//
+//  GetAllObjectsFromParseHelper.h
+//  TrailKeeper
+//
+//  Created by Jeremey Rodriguez on 10/30/15.
+//  Copyright © 2015 Jeremey Rodriguez. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import "Trails.h"
+#import "TrailStatus.h"
+#import "AuthorizedCommentors.h"
+#import "Comments.h"
+
+@interface GetAllObjectsFromParseHelper : PFObject
+
++(void)RefreshTrails;
++(void)RefreshTrailStatus;
++(void)RefreshAuthorizedCommentors;
++(void)RefreshComments;
+
+@end
---
+++ TrailKeeper/GetAllObjectsFromParseHelper.m
@@ -0,0 +1,57 @@
+//
+//  GetAllObjectsFromParseHelper.m
+//  TrailKeeper
+//
+//  Created by Jeremey Rodriguez on 10/30/15.
+//  Copyright © 2015 Jeremey Rodriguez. All rights reserved.
+//
+
+#import "GetAllObjectsFromParseHelper.h"
+
+@implementation GetAllObjectsFromParseHelper
+
++(void)RefreshTrails {
+    PFQuery *query = [PFQuery queryWithClassName:@"Trails"];
+    [[query findObjectsInBackground] continueWithSuccessBlock:^id(BFTask *task) {
+        return [[PFObject unpinAllObjectsInBackgroundWithName:@"Trails"] continueWithSuccessBlock:^id(BFTask *ignored) {
+            NSArray *trails = task.result;
+            NSLog(@"Trail Count from refresh %lu", (unsigned long)trails.count);
+            return [PFObject pinAllInBackground:trails withName:@"Trails"];
+        }];
+    }];
+}
+
++(void)RefreshTrailStatus {
+    PFQuery *query = [PFQuery queryWithClassName:@"TrailStatus"];
+    [[query findObjectsInBackground] continueWithSuccessBlock:^id(BFTask *task) {
+        return [[PFObject unpinAllObjectsInBackgroundWithName:@"TrailStatus"] continueWithSuccessBlock:^id(BFTask *ignored) {
+            NSArray *trailStatuses = task.result;
+            NSLog(@"TrailStatus Count from refresh %lu", (unsigned long)trailStatuses.count);
+            return [PFObject pinAllInBackground:trailStatuses withName:@"TrailStatus"];
+        }];
+    }];
+}
+
++(void)RefreshAuthorizedCommentors {
+    PFQuery *query = [PFQuery queryWithClassName:@"AuthorizedCommentors"];
+    [[query findObjectsInBackground] continueWithSuccessBlock:^id(BFTask *task) {
+        return [[PFObject unpinAllObjectsInBackgroundWithName:@"AuthorizedCommentors"] continueWithSuccessBlock:^id(BFTask *ignored) {
+            NSArray *commentors = task.result;
+            NSLog(@"Commentor Count from refresh %lu", (unsigned long)commentors.count);
+            return [PFObject pinAllInBackground:commentors withName:@"AuthorizedCommentors"];
+        }];
+    }];
+}
+
++(void)RefreshComments {
+    PFQuery *query = [PFQuery queryWithClassName:@"Comments"];
+    [[query findObjectsInBackground] continueWithSuccessBlock:^id(BFTask *task) {
+        return [[PFObject unpinAllObjectsInBackgroundWithName:@"Comments"] continueWithSuccessBlock:^id(BFTask *ignored) {
+            NSArray *comments = task.result;
+            NSLog(@"Comment Count from refresh %lu", (unsigned long)comments.count);
+            return [PFObject pinAllInBackground:comments withName:@"Comments"];
+        }];
+    }];
+}
+
+@end
